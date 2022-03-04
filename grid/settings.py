07736e3eb0c8aec14e192f9ddbb781210ad165f7
gbemii <gbengist@gmail.com>
2022-03-04 14:16:26 +0100

    main commit

--- grid/settings.py
+++ grid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h*#pevhge3fdc-1+so0=6t0083q(g0(#lk7w+9_7^v-c$3%58w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'gridpam.herokuapp.com']
 
 
 # Application definition
